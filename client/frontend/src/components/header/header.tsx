06d86d0dfaeeb6caf2a426391813c0df015c7164
kam-stand <kamrul.hassan@stonybrook.edu>
2024-05-18 01:53:01 -0400

    Created the header. need to finish the Footer and then pages

--- client/frontend/src/components/header/header.tsx
+++ client/frontend/src/components/header/header.tsx
@@ -1,19 +1,15 @@
-import React from "react";
 import "./header.css";
-
 export default function Header() {
   return (
     <div className="Header">
-      <div className="logo">Logo</div>
+      <div className="logo">
+        <h1>ProNotion</h1>
+      </div>
       <div className="Header_nav">
         <div className="Header_nav_item">
-          <a>Home</a>
-        </div>
-        <div className="Header_nav_item">
-          <a>About</a>
-        </div>
-        <div className="Header_nav_item">
-          <a>Pricing</a>
+          <a href="/Home">Home</a>
+          <a href="About">About</a>
+          <a href="/pricing">Pricing</a>
         </div>
       </div>
     </div>
