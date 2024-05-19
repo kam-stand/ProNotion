b2ac8909567654cdfb3dac1d931dfd6290afa0fe
kam-stand <kamrul.hassan@stonybrook.edu>
2024-05-19 18:17:37 -0400

    Refactoring about and started pricing

--- client/frontend/src/components/pages/about.tsx
+++ client/frontend/src/components/pages/about.tsx
@@ -1,41 +1,31 @@
 import "././pages_styling_theme/about.css";
+
 export default function About() {
   return (
-    <div className="parent">
-      <div className="About">
-        <div className="title">
+    <div className="Parent">
+      <div className="card">
+        <div>
           <h1>About</h1>
-          <h2>At Pronotion we believe in simplicity</h2>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-              temporibus vel sit, officiis perferendis cum quas dolores aliquam
-              distinctio commodi, eum iure. Iste consequuntur molestias
-              veritatis ducimus expedita minus reprehenderit.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Inventore ab saepe labore mollitia sed porro sapiente accusamus
-              nulla corrupti, blanditiis officia. Iusto necessitatibus rerum ex
-              ipsum sunt a, ipsa quod? lorem impsun dolor sit amet, consectetur
-              adipisicing elit. Inventore ab saepe labore mollitia sed porro
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-              temporibus vel sit, officiis perferendis cum quas dolores aliquam
-              distinctio commodi, eum iure. Iste consequuntur molestias
-              veritatis ducimus expedita minus reprehenderit.
-            </p>
-          </div>
         </div>
-        <div className="image">
-          <img
-            src="https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?cs=srgb&dl=pexels-cottonbro-4065891.jpg&fm=jpg"
-            alt="logo"
-            width="700px"
-            border-radius="20%"
-          />
+        <div>
+          <h2>Subheading</h2>
         </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero odio
+            nesciunt consequatur blanditiis rem assumenda tempora quasi nulla?
+            Natus eum vel deleniti quas neque itaque, delectus quis culpa
+            provident dolorem!
+          </p>
+        </div>
+      </div>
+      <div className="hero_image">
+        <img
+          src="https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?cs=srgb&dl=pexels-cottonbro-4065891.jpg&fm=jpg"
+          alt="logo"
+          width="700px"
+          border-radius="20%"
+        />
       </div>
     </div>
   );
--- client/frontend/src/components/pages/pricing.tsx
+++ client/frontend/src/components/pages/pricing.tsx
@@ -1,5 +1,29 @@
+import "./pages_styling_theme/pricing.css";
 export default function Pricing() {
-  return <></>;
+  return (
+    <>
+      <div className="Parent">
+        <div className="header">
+          <h1>Pricing</h1>
+        </div>
+        <div className="pricing">
+          <div className="card">1</div>
+          <div className="card">2</div>
+          <div className="card">3</div>
+        </div>
+        <div className="testimonial">
+          <div className="card">1</div>
+          <div className="card">2</div>
+          <div className="card">3</div>
+        </div>
+        <div className="payments">
+          <div className="card">1</div>
+          <div className="card">2</div>
+          <div className="card">3</div>
+        </div>
+      </div>
+    </>
+  );
 }
 /**
  * 1. 3 cards
