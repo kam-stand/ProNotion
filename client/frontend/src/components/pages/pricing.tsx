dda31887e0010dfb3b3b61976fef5ac0459336b3
kam-stand <kamrul.hassan@stonybrook.edu>
2024-05-25 22:35:39 -0400

    Added pricing tiers. need to finish testimonials and make payment images better

--- client/frontend/src/components/pages/pricing.tsx
+++ client/frontend/src/components/pages/pricing.tsx
@@ -11,11 +11,62 @@ export default function Pricing() {
           <h1>Pricing</h1>
         </div>
         <div className="pricing">
-          <div className="card">1</div>
-          <div className="card">2</div>
-          <div className="card">3</div>
+          <div className="card">
+            <div className="price-context">
+              <h1>Basic</h1>
+              <h2>$10/month</h2>
+              <div className="info">
+                <p>5 users included</p>
+                <div>
+                  <ol>
+                    <ul>basic feature</ul>
+                    <ul>not included feature</ul>
+                    <ul>not included feature</ul>
+                    <ul>not included feature</ul>
+                  </ol>
+                </div>
+              </div>
+              <button className="button">Purchase</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="price-context">
+              <h1>Professional</h1>
+              <h2>$20/month</h2>
+              <div className="info">
+                <p>5 users included</p>
+                <div>
+                  <ol>
+                    <ul>Professional feature</ul>
+                    <ul>Professional feature</ul>
+                    <ul>Professional feature</ul>
+                    <ul>Professional feature</ul>
+                  </ol>
+                </div>
+              </div>
+              <button className="button">Purchase</button>
+            </div>
+          </div>
+          <div className="card">
+            <div className="price-context">
+              <h1>Enterprise</h1>
+              <h2>$50/month</h2>
+              <div className="info">
+                <p>100+ users included</p>
+                <div>
+                  <ol>
+                    <ul>Enterprise feature</ul>
+                    <ul>Enterprise feature</ul>
+                    <ul>Enterprise feature</ul>
+                    <ul>Enterprise feature</ul>
+                  </ol>
+                </div>
+              </div>
+              <button className="button">Purchase</button>
+            </div>
+          </div>
         </div>
-        <h3>Testimonial</h3>
+        <h2>Testimonial</h2>
         <div className="testimonial">
           <div className="card">1</div>
           <div className="card">2</div>
@@ -33,6 +84,20 @@ export default function Pricing() {
             <FaCcMastercard size="80px" />
           </div>
         </div>
+      </div>
+      <div className="img">
+        <img
+          height="210px"
+          alt="img1"
+          width="fit-content"
+          src="https://images.thdstatic.com/productImages/d876ff26-5e8e-4585-98e5-9e50c621f25f/svn/black-wall-sculptures-wa1748w-64_600.jpg"
+        />
+        <img
+          alt="img2"
+          height="200px"
+          width="fit-content"
+          src="https://img.fruugo.com/product/7/87/969719877_max.jpg"
+        />
       </div>
     </>
   );
