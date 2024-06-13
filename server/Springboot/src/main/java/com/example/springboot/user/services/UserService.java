bac71a58e6f24923e23ccab2d359ea744ba82860
kam-stand <kamrul.hassan@stonybrook.edu>
2024-06-13 00:17:34 -0400

    Added a function to user service layer. May need to add DAO and DTO layers

--- server/Springboot/src/main/java/com/example/springboot/user/services/UserService.java
+++ server/Springboot/src/main/java/com/example/springboot/user/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.springboot.user.model.User;
 import com.example.springboot.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.springboot.roles.Membership;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,20 +13,40 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
     public User save(User user) {
         return userRepository.save(user);
     }
+
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
+
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
-    public User update(User user) {
-        return userRepository.save(user);
+
+    public User update(User user, Long id) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    public User changeMemberShip(long id, String membership) {
+        User user = userRepository.findById(id).orElse(null);
+        Membership membership1 = Membership.valueOf(membership.toUpperCase());
+        if (user != null) {
+            user.setMembership(membership1);
+            return userRepository.save(user);
+        }
+        return null;
+
     }
 
 }
