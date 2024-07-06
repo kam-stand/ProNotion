a239ae4d8bb568b1e7c3a348224f38a259c6cde8
kam-stand <kamrul.hassan@stonybrook.edu>
2024-07-06 14:20:14 -0400

    Created task and user controller much more cleaner - next is tags and events then signup

---
+++ server/Springboot/src/main/java/com/example/springboot/model/User.java
@@ -0,0 +1,107 @@
+package com.example.springboot.model;
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userId;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Column(name = "dateCreated",
+            updatable = false)
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @ManyToMany(mappedBy = "assigned_to")
+    private List<Task> tasks;
+
+
+
+    public User(){
+
+    }
+
+    public User(String firstName, String lastName, String email, String password, Date dateCreated, List<Task> tasks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.dateCreated = dateCreated;
+        this.tasks = tasks;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+
+}
