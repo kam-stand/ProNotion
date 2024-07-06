e61ecdde2ffdacd54d14201d8cfea981c6f2736f
kam-stand <kamrul.hassan@stonybrook.edu>
2024-07-06 00:11:59 -0400

    Dropped all modelsm repos, services and controllers

--- server/Springboot/src/main/java/com/example/springboot/roles/Membership.java
+++
@@ -1,6 +0,0 @@
-
-package com.example.springboot.roles;
-
-public enum Membership {
-    BASIC, ADVANCED, ENTERPRISE
-}
--- server/Springboot/src/main/java/com/example/springboot/roles/Role.java
+++
@@ -1,6 +0,0 @@
-
-package com.example.springboot.roles;
-
-public enum Role {
-    USER, ADMIN, GUEST
-}
