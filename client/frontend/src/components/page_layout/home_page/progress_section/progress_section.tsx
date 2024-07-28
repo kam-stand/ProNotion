99c95008e98cd5a1a63df68b2f57cb0d4c824fa3
kam-stand <kamrul.hassan@stonybrook.edu>
2024-07-28 16:53:55 -0400

    starting over

--- client/frontend/src/components/page_layout/home_page/progress_section/progress_section.tsx
+++
@@ -1,9 +0,0 @@
-import "./progress_section.css";
-
-export default function ProgressSection() {
-  return (
-    <div className="progress-section">
-      <div className="progress-section-item">progress</div>
-    </div>
-  );
-}
--- client/frontend/src/components/pages/login_signup/signup.tsx
+++
@@ -1,9 +0,0 @@
-export default function SignUp() {
-  return (
-    <div className="signup">
-      <div className="signup-item">
-        <h1>Sign Up</h1>
-      </div>
-    </div>
-  );
-}
