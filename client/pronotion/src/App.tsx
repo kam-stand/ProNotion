0e1dac114ddcf981bf21bb614be0b9c2e501979d
kam-stand <kamrul.hassan@stonybrook.edu>
2024-07-30 21:56:09 -0400

    created header and footer

--- client/pronotion/src/App.tsx
+++ client/pronotion/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Footer from "./page_layouts/footer";
 import Header from "./page_layouts/header";
 
 function App() {
   return (
     <>
       <Header />
+
+      <Footer />
     </>
   );
 }
--- client/pronotion/src/page_layouts/footer.tsx
+++ client/pronotion/src/page_layouts/footer.tsx
@@ -1,16 +1,36 @@
+import React from "react";
+import "../styles/footer.css";
+
 export default function Footer() {
   return (
     <>
-      <div>
-        <h1>ProNotion </h1>
-        <h3>
-          ProNotion is a platform to share your ideas, thoughts, and knowledge
-          with the world.{" "}
-        </h3>
+      <div className="footer-parent">
+        <div className="logo">
+          <img
+            className="logo-image"
+            src={require("../assets/pronotion-high-resolution-logo-black.png")}
+            alt="ProNotion Logo"
+          />
+        </div>
+        <div className="social-media-links">
+          <a href="https://www.linkedin.com/in/kamrul-hassan-9a97a71b2?trk=public_profile_browsemap_profile-result-card_result-card_full-click">
+            <img src={require("../assets/download.png")} alt="LinkedIn" />
+          </a>
+          <a href="mailto:kh84590@gmail.com">
+            <img src={require("../assets/gmail.png")} alt="Gmail" />
+          </a>
+          <a href="https://github.com/kam-stand/ProNotion">
+            <img src={require("../assets/github.png")} alt="GitHub" />
+          </a>
+        </div>
 
         <div>
-          <button>Sign Up</button>
-          <button>Sign In</button>
+          <div className="utils">
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/terms">Terms</a>
+            <a href="/privacy">Privacy</a>
+          </div>
         </div>
       </div>
     </>
