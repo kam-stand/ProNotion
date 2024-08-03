83d71e655a42f7024f8d0c01b272da0f380750c9
kam-stand <kamrul.hassan@stonybrook.edu>
2024-08-03 16:38:36 -0400

    tested registration and post request. it works. needed css and more management

--- client/pronotion/src/pages/auth_page.tsx
+++ client/pronotion/src/pages/auth_page.tsx
@@ -1,23 +1,29 @@
 import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
+
 export default function AuthPage() {
-  const [auth, setAuth] = useState("");
+  const [auth, setAuth] = useState<null | "login" | "signup">(null);
+
+  const handleAuth = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const option = event.currentTarget.innerText.toLowerCase();
+    setAuth(option as "login" | "signup");
+  };
+
   return (
     <div className="auth-parent">
       <h1>Join us at ProNotion</h1>
       <p>Returning Users</p>
       <div className="button">
-        <button onClick={() => setAuth("login")}>Login</button>
+        <button onClick={handleAuth}>Login</button>
       </div>
       <p>Create an account</p>
       <div className="button">
-        <button onClick={() => setAuth("signup")}>Signup</button>
+        <button onClick={handleAuth}>Signup</button>
       </div>
-      {/* if the auth is equal to login render the Login Component  OtherWise render SignupComponent the functions*/}
       {auth === "login" ? (
-        loginComponent()
+        <LoginComponent />
       ) : auth === "signup" ? (
-        signupComponent()
+        <SignupComponent />
       ) : (
         <></>
       )}
@@ -41,7 +47,8 @@ const signup = async (
   email: string,
   password: string,
   firstName: string,
-  lastName: string
+  lastName: string,
+  role: string
 ) => {
   try {
     const response = await axios.post("http://localhost:8080/auth/register", {
@@ -49,6 +56,7 @@ const signup = async (
       password,
       firstName,
       lastName,
+      role,
     });
     console.log(response.data);
   } catch (error) {
@@ -56,7 +64,7 @@ const signup = async (
   }
 };
 
-const loginComponent = () => {
+const LoginComponent = () => {
   return (
     <>
       <h1>Login</h1>
@@ -69,15 +77,50 @@ const loginComponent = () => {
   );
 };
 
-const signupComponent = () => {
+const SignupComponent = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+
+  const handleRegistration = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // check if all fields are filled
+    if (email && password && firstName && lastName) {
+      signup(email, password, firstName, lastName, "USER");
+    } else {
+      alert("Please fill all fields");
+    }
+  };
+
   return (
     <>
       <h1>Signup</h1>
-      <form>
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form onSubmit={handleRegistration}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
         <button>Signup</button>
       </form>
     </>
