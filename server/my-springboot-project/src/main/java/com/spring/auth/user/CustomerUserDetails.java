f46a5867457cd6eeb7897e36d28db2360b7c3163
kam-stand <kamrul.hassan@stonybrook.edu>
2024-08-25 12:49:10 -0400

    Created Auth folder will reimplement methods and security measures

---
+++ server/my-springboot-project/src/main/java/com/spring/auth/user/CustomerUserDetails.java
@@ -0,0 +1,32 @@
+
+package com.spring.auth.user;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomerUserDetails  implements UserDetails{
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+
+
+
+    
+}
