748a9b0d05c9101db4dfe9b4590c5dd5a34f96cf
kam-stand <kamrul.hassan@stonybrook.edu>
2024-08-26 12:19:30 -0400

    created base layout for security need to test username and pass authentication then proceed to JWT

--- server/my-springboot-project/src/main/java/com/spring/auth/user/CustomUserService.java
+++ server/my-springboot-project/src/main/java/com/spring/auth/user/CustomUserService.java
@@ -1,15 +1,40 @@
 package com.spring.auth.user;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.spring.model.User;
+import com.spring.repository.UserRepository;
+
+@Service
 public class CustomUserService implements UserDetailsService{
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    public CustomUserService(){
+
+    }
+
+
+    @Autowired
+    public CustomUserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        Optional<User> userDto = userRepository.findByEmail(username);
+        User user = userDto.orElseThrow(() -> new UsernameNotFoundException("Invalid username or password"));
+
+        return new CustomerUserDetails(user);
+       
     }
 
 }
