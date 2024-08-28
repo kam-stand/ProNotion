817d75a2d8fccc3fe86124c13f343240da089f91
kam-stand <kamrul.hassan@stonybrook.edu>
2024-08-28 11:41:06 -0400

    fixed username password with dao, understand more

--- server/my-springboot-project/src/main/java/com/spring/auth/Security.java
+++ server/my-springboot-project/src/main/java/com/spring/auth/Security.java
@@ -2,18 +2,28 @@
 
 package com.spring.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.spring.auth.user.CustomUserService;
+
 @Configuration
 @EnableWebSecurity
 public class Security {
+
+    @Autowired
+    CustomUserService customUserService;
 
 
     @Bean
@@ -24,6 +34,8 @@ public class Security {
             auth -> auth
             .requestMatchers("/auth/**").permitAll()
             .anyRequest().authenticated())
+            .authenticationProvider(authenticationProvider())
+            .authenticationManager(authenticationManager(new AuthenticationConfiguration()))
             .httpBasic(Customizer.withDefaults());
             return http.build();
 
@@ -33,6 +45,20 @@ public class Security {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(customUserService);
+        return daoAuthenticationProvider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     
--- server/my-springboot-project/src/main/java/com/spring/controller/AuthController.java
+++ server/my-springboot-project/src/main/java/com/spring/controller/AuthController.java
@@ -3,26 +3,46 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.spring.dto.UserDto;
 import com.spring.dto.auth.LoginDto;
 import com.spring.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("/login")
     public String login(@RequestBody LoginDto LoginDto){
-        return "login";
+
+        
+        
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(LoginDto.getUsername(), LoginDto.getPassword()));
+        
+        if(!authentication.isAuthenticated()){
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            authentication.setAuthenticated(true);
+    
+        }
+        
+        
+
+        return "youre logged in";
     }
 
     @PostMapping("/register")
