a9c1c82b43ef0e092a048f0c3e836def35724fb6
kamstand <kamrul.hassan@stonybrook.edu>
2025-06-16 21:01:11 -0400

    added css for welcome page component

---
+++ client/Pro-Notion/src/pages/welcome_page.tsx
@@ -0,0 +1,40 @@
+import "./syles/welompe_page.css";
+export default function WelcomePage() {
+  return (
+    <div className="WelcomePage">
+      <div className="welcome-header">
+        <h1>
+          Welcome to Pro Notion!
+          <br />
+          <span className="subtitle">Your AI-powered Task Assistant</span>
+        </h1>
+        <div>
+          <p>
+            Pro Notion is an AI-powered assistant designed to enhance your
+            productivity and streamline your workflow within Notion.
+          </p>
+          <p>
+            With Pro Notion, you can easily manage tasks, set reminders, and
+            automate repetitive actions, all while enjoying a seamless
+            integration with your Notion workspace.
+          </p>
+        </div>
+      </div>
+      <div className="welcome-header">
+        <h1>
+          Welcome to Pro Notion!
+          <br />
+          <span className="subtitle">Your AI-powered Notion Assistant</span>
+        </h1>
+      </div>
+
+      <div className="welcome-header">
+        <h1>
+          Welcome to Pro Notion!
+          <br />
+          <span className="subtitle">Your AI-powered Notion Assistant</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
